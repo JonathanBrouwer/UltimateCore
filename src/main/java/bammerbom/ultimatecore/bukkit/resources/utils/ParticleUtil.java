1c290b62912f9d2aa2c1e82a6188a123d5d27d79
Bammerbom <j.brouwer@filternet.nl>
2015-01-31 16:40:26 +0100

    Fix missing import in particle util.

--- src/main/java/bammerbom/ultimatecore/bukkit/resources/utils/ParticleUtil.java
+++ src/main/java/bammerbom/ultimatecore/bukkit/resources/utils/ParticleUtil.java
@@ -26,6 +26,7 @@ package bammerbom.ultimatecore.bukkit.resources.utils;
 import bammerbom.ultimatecore.bukkit.resources.utils.ReflectionUtils.PackageType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
