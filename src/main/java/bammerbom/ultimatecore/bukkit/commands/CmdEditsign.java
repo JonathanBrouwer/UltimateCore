af1aa29e7b5f663f2fedec8b9f8f077cb311299d
Bammerbom <j.brouwer@filternet.nl>
2015-04-25 09:47:49 +0200

    Check if player has access before editing sign

--- src/main/java/bammerbom/ultimatecore/bukkit/commands/CmdEditsign.java
+++ src/main/java/bammerbom/ultimatecore/bukkit/commands/CmdEditsign.java
@@ -29,12 +29,14 @@ import bammerbom.ultimatecore.bukkit.resources.utils.LocationUtil;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class CmdEditsign implements UltimateCommand {
@@ -75,6 +77,12 @@ public class CmdEditsign implements UltimateCommand {
             } else {
                 r.sendMes(cs, "editsignNoSignB");
             }
+            return;
+        }
+        BlockBreakEvent ev = new BlockBreakEvent(b, p);
+        Bukkit.getPluginManager().callEvent(ev);
+        if (ev.isCancelled()) {
+            r.sendMes(cs, "editsignNoAccess");
             return;
         }
         Sign s = (Sign) b.getState();
