e0da8ccf0f13c3598fb314e63f1dd2031ae10a83
Jonathan <j.brouwer@filternet.nl>
2016-04-09 14:14:23 +0200

    Fix action bars

--- src/main/java/bammerbom/ultimatecore/bukkit/resources/utils/TitleUtil.java
+++ src/main/java/bammerbom/ultimatecore/bukkit/resources/utils/TitleUtil.java
@@ -40,43 +40,44 @@ public class TitleUtil {
         new Title(title, subtitle, fadeIn, stay, fadeOut).send(p);
     }
 
-    public static void sendActionBar(Player player, String message) {
+    public static void sendActionBar(Player player, String message)
+    {
         String nmsver = Bukkit.getServer().getClass().getPackage().getName();
         nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
-            try
-            {
-                Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
-                Object p = c1.cast(player);
-                Object ppoc;
-                Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
-                Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
-                if ((nmsver.equalsIgnoreCase("v1_8_R1")) || (!nmsver.startsWith("v1_8_")))
-                {
-                    Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
-                    Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                    Method m3 = c2.getDeclaredMethod("a", String.class);
-                    Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-                    ppoc = c4.getConstructor(new Class[]{c3, Byte.TYPE}).newInstance(cbc, Byte.valueOf("2"));
-                }
-                else
-                {
-                    Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
-                    Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                    Object o = c2.getConstructor(new Class[]{String.class}).newInstance(message);
-                    ppoc = c4.getConstructor(new Class[]{c3, Byte.TYPE}).newInstance(o, Byte.valueOf("2"));
-                }
-                Method m1 = c1.getDeclaredMethod("getHandle");
-                Object h = m1.invoke(p);
-                Field f1 = h.getClass().getDeclaredField("playerConnection");
-                Object pc = f1.get(h);
-                Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
-                m5.invoke(pc, ppoc);
-            }
-            catch (Exception ex)
-            {
-                ErrorLogger.log(ex, "Failed to send action bar.");
-            }
+        try
+        {
+            Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+            Object p = c1.cast(player);
 
+            Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
+            Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+            Object ppoc;
+            if (((nmsver.equalsIgnoreCase("v1_8_R1")) || (!nmsver.startsWith("v1_8_"))) && (!nmsver.startsWith("v1_9_")))
+            {
+                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
+                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+                Method m3 = c2.getDeclaredMethod("a", new Class[] { String.class });
+                Object cbc = c3.cast(m3.invoke(c2, new Object[] { "{\"text\": \"" + message + "\"}" }));
+                ppoc = c4.getConstructor(new Class[] { c3, Byte.TYPE }).newInstance(new Object[] { cbc, Byte.valueOf("2") });
+            }
+            else
+            {
+                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
+                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+                Object o = c2.getConstructor(new Class[] { String.class }).newInstance(new Object[] { message });
+                ppoc = c4.getConstructor(new Class[] { c3, Byte.TYPE }).newInstance(new Object[] { o, Byte.valueOf("2") });
+            }
+            Method m1 = c1.getDeclaredMethod("getHandle", new Class[0]);
+            Object h = m1.invoke(p, new Object[0]);
+            Field f1 = h.getClass().getDeclaredField("playerConnection");
+            Object pc = f1.get(h);
+            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", new Class[] { c5 });
+            m5.invoke(pc, new Object[] { ppoc });
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
 
