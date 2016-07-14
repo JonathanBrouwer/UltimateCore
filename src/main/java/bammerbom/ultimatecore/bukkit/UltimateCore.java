7a8fe6118dcf5ec6e5ce89a000479592e6f78f3b
Jonathan <j.brouwer@filternet.nl>
2016-07-14 09:44:07 +0200

    How did this get commited

--- src/main/java/bammerbom/ultimatecore/bukkit/UltimateCore.java
+++ src/main/java/bammerbom/ultimatecore/bukkit/UltimateCore.java
@@ -200,6 +200,5 @@ public class UltimateCore extends JavaPlugin implements Listener {
     }
 
     public void test() {
-        ErrorLogger.log(new NullPointerException(), "Wat kut, een error :(");
     }
 }
