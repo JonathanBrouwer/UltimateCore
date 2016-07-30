6e880776f19406ac63b497ff4c2bb5ac589c0d87
Jonathan <j.brouwer@filternet.nl>
2016-07-30 11:46:10 +0200

    Add events

---
+++ src/main/java/bammerbom/ultimatecore/sponge/api/event/module/ModuleEvent.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of UltimateCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Bammerbom
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package bammerbom.ultimatecore.sponge.api.event.module;
+
+import bammerbom.ultimatecore.sponge.api.module.Module;
+import org.spongepowered.api.event.Event;
+import org.spongepowered.api.event.cause.Cause;
+
+/**
+ * A {@link ModuleEvent} is an event where a {@link Module} is involved.
+ */
+public class ModuleEvent implements Event {
+    private Cause cause;
+    private Module module;
+
+    public ModuleEvent(Module module, Cause cause){
+        this.module = module;
+        this.cause = cause;
+    }
+
+    /**
+     * Get the cause for the event.
+     *
+     * @return The last cause
+     */
+    @Override
+    public Cause getCause() {
+        return cause;
+    }
+
+    /**
+     * Get the {@link Module} for the event
+     *
+     * @return The module
+     */
+    public Module getModule() {
+        return module;
+    }
+}
---
+++ src/main/java/bammerbom/ultimatecore/sponge/api/event/module/ModuleRegisterEvent.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of UltimateCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Bammerbom
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package bammerbom.ultimatecore.sponge.api.event.module;
+
+import bammerbom.ultimatecore.sponge.api.module.Module;
+import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.Event;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+
+/**
+ * This is called before a module is registered.
+ * Normally, this is called during {@link GamePreInitializationEvent}
+ *
+ * When this is cancelled, the module will not be registered.
+ */
+public class ModuleRegisterEvent extends ModuleEvent implements Cancellable {
+    public ModuleRegisterEvent(Module module, Cause cause) {
+        super(module, cause);
+    }
+
+    /**
+     * Gets if the {@link Event} has been cancelled.
+     *
+     * @return Is this event cancelled
+     */
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    /**
+     * Sets the cancelled state of the {@link Event}.
+     *
+     * @param cancel The new cancelled state
+     */
+    @Override
+    public void setCancelled(boolean cancel) {
+
+    }
+}
