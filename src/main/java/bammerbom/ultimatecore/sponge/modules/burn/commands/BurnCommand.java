6cf41adf690d1b29c756ce7cab7295539ca27b40
TheAsher <eslistavenga@hotmail.com>
2016-08-04 17:17:10 +0200

    Added burn command
    Com get the Mamera I did it totally legit all by myself. No help from
    bam.... ;)

---
+++ src/main/java/bammerbom/ultimatecore/sponge/modules/burn/commands/BurnCommand.java
@@ -0,0 +1,117 @@
+/*
+ * This file is part of UltimateCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Bammerbom
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package bammerbom.ultimatecore.sponge.modules.burn.commands;
+
+import bammerbom.ultimatecore.sponge.api.command.Command;
+import bammerbom.ultimatecore.sponge.api.module.Module;
+import bammerbom.ultimatecore.sponge.api.module.Modules;
+import bammerbom.ultimatecore.sponge.api.permission.Permission;
+import bammerbom.ultimatecore.sponge.modules.burn.api.BurnPermissions;
+import bammerbom.ultimatecore.sponge.utils.Messages;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BurnCommand implements Command {
+    @Override
+    public Module getModule() {
+        return Modules.BURN.get();
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "burn";
+    }
+
+    @Override
+    public Permission getPermission() {
+        return BurnPermissions.UC_BURN;
+    }
+
+    @Override
+    public List<Permission> getPermissions() {
+        return Arrays.asList(BurnPermissions.UC_BURN);
+    }
+
+    @Override
+    public Text getUsage() {
+        return Messages.getFormatted("burn.command.burn.usage");
+    }
+
+    @Override
+    public Text getShortDescription() {
+        return Messages.getFormatted("burn.command.burn.shortdescription");
+    }
+
+    @Override
+    public Text getLongDescription() {
+        return Messages.getFormatted("burn.command.burn.longdescription");
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Arrays.asList("fire" , "ignite");
+    }
+
+    @Override
+    public CommandResult run(CommandSource sender, String[] args) {
+        if (!sender.hasPermission(BurnPermissions.UC_BURN.get())) {
+            sender.sendMessage(Messages.getFormatted("core.nopermissions"));
+            return CommandResult.empty();
+        }
+        if (args.length == 0) {
+            sender.sendMessage(getUsage());
+            return CommandResult.empty();
+        }
+        Player t;
+        if (Sponge.getServer().getPlayer(args[0]).isPresent() ) {
+            t = Sponge.getServer().getPlayer(args[0]).get();
+        } else {
+            sender.sendMessage(Messages.getFormatted("core.playernotfound" , "%player%" , args[0]));
+            return CommandResult.empty();
+        }
+        Double time = 10.0;
+        if (args.length >= 2) {
+            try{
+                time = Double.parseDouble(args[1]);
+            } catch(Exception ex) {
+
+            }
+        }
+        t.offer(Keys.FIRE_TICKS,Double.valueOf(time*20).intValue());
+        sender.sendMessage(Messages.getFormatted("burn.command.burn.success", "%player%", t.getName(), "%time%", time ));
+
+    return CommandResult.success(); }
+
+    @Override
+    public List<String> onTabComplete(CommandSource sender, String[] args, String curs, Integer curn) {
+        return null;
+    }
+}
