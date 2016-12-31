4bb14f1693fa2763cdba0009821460e4d469e15d
Jonathan <j.brouwer@filternet.nl>
2016-12-31 16:00:17 +0100

    Item module, more command

---
+++ src/main/java/bammerbom/ultimatecore/sponge/modules/item/commands/MoreCommand.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of UltimateCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Bammerbom
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package bammerbom.ultimatecore.sponge.modules.item.commands;
+
+import bammerbom.ultimatecore.sponge.api.command.Command;
+import bammerbom.ultimatecore.sponge.api.module.Module;
+import bammerbom.ultimatecore.sponge.api.module.Modules;
+import bammerbom.ultimatecore.sponge.api.permission.Permission;
+import bammerbom.ultimatecore.sponge.modules.item.api.ItemPermissions;
+import bammerbom.ultimatecore.sponge.utils.Messages;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.data.type.HandTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MoreCommand implements Command {
+    @Override
+    public Module getModule() {
+        return Modules.ITEM.get();
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "more";
+    }
+
+    @Override
+    public Permission getPermission() {
+        return ItemPermissions.UC_MORE;
+    }
+
+    @Override
+    public List<Permission> getPermissions() {
+        return Arrays.asList(ItemPermissions.UC_MORE);
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Arrays.asList("more");
+    }
+
+    @Override
+    public CommandResult run(CommandSource sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Messages.getFormatted("core.noplayer", "%source%", sender.getName()));
+            return CommandResult.empty();
+        }
+        Player p = (Player) sender;
+        if (!p.getItemInHand(HandTypes.MAIN_HAND).isPresent() || p.getItemInHand(HandTypes.MAIN_HAND).get().getItem().equals(ItemTypes.NONE)) {
+            p.sendMessage(Messages.getFormatted("item.command.more.nohand"));
+            return CommandResult.empty();
+        }
+        ItemStack stack = p.getItemInHand(HandTypes.MAIN_HAND).get();
+        stack.setQuantity(stack.getMaxStackQuantity());
+        p.setItemInHand(HandTypes.MAIN_HAND, stack);
+        p.sendMessage(Messages.getFormatted("item.command.more.success", "%amount%", stack.getMaxStackQuantity()));
+        return CommandResult.success();
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSource sender, String[] args, String curs, Integer curn) {
+        return null;
+    }
+}
